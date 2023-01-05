874f0cbc25bd66cf0d2cc598e36afacba1d7bb78
Mkrtich <100070929+mkrtic@users.noreply.github.com>
2023-01-05 20:32:08 +0400

    four day
    ej@ amboxjutyamb ashxatum e (miayn petqe css ov dizaynayin popoxutyunner anem)

--- Task management system/js/main.js
+++ Task management system/js/main.js
@@ -6,7 +6,11 @@ function censel(){
     document.getElementById("formAdd").style = "display: none;"
 }
 
-let obj,tasks,idTask,day,down,downColor;
+function censelEdit(){
+    document.getElementById("formEdit").style = "display: none;"
+}
+
+let obj,tasks,idTask,day,down,downColor,editDown;
 
 tasks = [
 
@@ -17,7 +21,7 @@ idTask = 0;
 day = new Date().toLocaleString().split(',')[0]; 
 
 down = document.getElementById("dropdown");
-
+editDown = document.getElementById("dropdownEdit");
 
 downColor = [
     "colorOne",
@@ -31,22 +35,23 @@ function delet(id){
     delete tasks[id];
 }
 
-// DynamicsCompressorNode
-
-function edit(id){
-    tasks[id] = {
-        id : idTask, 
-        taskName : document.getElementById("typeText").value,
-        taskDescription : document.getElementById("typeDescription").value,
-        tday : day,
-        stat : down.options[down.selectedIndex].text
-    }
-    document.getElementById("formAdd").style = 'display: block';
-    document.getElementById("typeText").value = "";
-    document.getElementById("typeDescription").value = "";
+function editAdd(ids){
+    objIndex = tasks.findIndex((obj => obj.id == ids));
+    document.getElementById("formEdit").style = "display: block;";
 }
 
-// DynamicsCompressorNode
+function edit() {
+    tasks[objIndex].taskName = document.getElementById("typeTextEdit").value;
+    tasks[objIndex].taskDescription = document.getElementById("typeDescriptionEdit").value;
+    tasks[objIndex].tday = day;
+    tasks[objIndex].stat = editDown.options[editDown.selectedIndex].text;
+    document.getElementById("formEdit").style = 'display: none';
+    document.getElementById("typeText").value = "";
+    document.getElementById("typeDescription").value = "";
+
+
+    document.getElementById(`${tasks[objIndex].id}`).innerHTML = "<li>" + tasks[objIndex].taskName + "</li>" + "<li>" + tasks[objIndex].taskDescription + "</li>" + "<li>" + tasks[objIndex].tday + "</li>" + `<li class='status ${downColor[editDown.options[editDown.selectedIndex].value]}' id='statusColor'>` + tasks[objIndex].stat + "</li>" + `<button onclick='delet(${objIndex})'>` + "X" + "</button>" + `<button onclick='editAdd(${objIndex})'>` + "Edit" + "</button>";
+}
 
 function submit() {
     obj = {
@@ -60,7 +65,7 @@ function submit() {
     let ul = document.createElement('ul');
     ul.setAttribute("class", "list-task");
     ul.setAttribute("id", `${idTask}`);
-    ul.innerHTML = "<li>" + tasks[idTask].id + "</li>" + "<li>" + tasks[idTask].taskName + "</li>" + "<li>" + tasks[idTask].taskDescription + "</li>" + "<li>" + tasks[idTask].tday + "</li>" + `<li class='status ${downColor[down.options[down.selectedIndex].value]}' id='statusColor'>` + tasks[idTask].stat + "</li>" + `<button onclick='delet(${idTask})'>` + "X" + "</button>" + `<button onclick='edit(${idTask})'>` + "Edit" + "</button>";
+    ul.innerHTML = "<li>" + tasks[idTask].taskName + "</li>" + "<li>" + tasks[idTask].taskDescription + "</li>" + "<li>" + tasks[idTask].tday + "</li>" + `<li class='status ${downColor[down.options[down.selectedIndex].value]}' id='statusColor'>` + tasks[idTask].stat + "</li>" + `<button onclick='delet(${idTask})'>` + "X" + "</button>" + `<button onclick='editAdd(${idTask})'>` + "Edit" + "</button>";
     document.getElementById("array").appendChild(ul);
     document.getElementById("formAdd").style = 'display: none';
     document.getElementById("typeText").value = "";
